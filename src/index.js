c02fad7fe037b470f85fef97a1d5c9c9352e982b
sibo gumbo <precygumbo@gmail.com>
2024-08-12 18:34:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let apiKey = "dc13349b33f710cte63cf40fe59aa2o5";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value} `;
   let context =
-    "You are a Poem generator expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Do not include a heading. Make sure to follow the user instructions";
+    "You are a Poem generator expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Do not include a heading. Sign the poem with 'Sibo Gumbo' inside a <strong></strong> element at the end of the poem. Make sure to follow the user instructions";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating poem");
