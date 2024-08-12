834f85030a4f858829e239a98a208f47f269abb6
sibo gumbo <precygumbo@gmail.com>
2024-08-12 18:25:57 +0200

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: ["As the dear pantenth for the water"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "dc13349b33f710cte63cf40fe59aa2o5";
+  let prompt = `User instructions: Generate a poem about ${instructionsInput.value} `;
+  let context =
+    "You are a Poem generator expert and you love to write short poems. Your mission is to generate a 4 line poem in basic HTML and seperate each line with a <br />. Do not include a heading. Make sure to follow the user instructions";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let generateFormElement = document.querySelector("#poem-generator-form");
